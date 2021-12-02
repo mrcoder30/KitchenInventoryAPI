ca04536a0ed4f019e630887306699a6e2ea8af2a
Daniel <danielw288@icloud.com>
2021-12-02 22:56:07 +0000

    populated test database and data

---
+++ src/main/resources/test-data.sql
@@ -0,0 +1,2 @@
+insert into product (name, price, quantity) values ('eggs', 2, 12);
+insert into product (name, price, quantity) values ('chips', 1, 30);
---
+++ src/main/resources/test-schema.sql
@@ -0,0 +1,2 @@
+DROP TABLE IF EXISTS Product;
+CREATE TABLE Product (id integer auto_increment, name varchar(255), price float, quantity float, primary key (id)); 
