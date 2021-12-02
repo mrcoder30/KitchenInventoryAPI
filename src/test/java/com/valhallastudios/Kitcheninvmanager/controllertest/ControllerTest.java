8a58f76fd1c2d1e87e6787a414b79a6d17f11f54
Daniel <danielw288@icloud.com>
2021-12-02 23:02:16 +0000

    tests completed and passed

--- src/test/java/com/valhallastudios/Kitcheninvmanager/controllertest/ControllerTest.java
+++ src/test/java/com/valhallastudios/Kitcheninvmanager/controllertest/ControllerTest.java
@@ -1,5 +1,87 @@
 package com.valhallastudios.Kitcheninvmanager.controllertest;
 
-public class ControllerTest {
+import java.util.List;
 
+import javax.swing.text.AbstractDocument.Content;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.ResultMatcher;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.valhallacoders.kitcheninventory.model.Product;
+
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
+@Sql(scripts = {"classpath:test-schema.sql", "classpath:testdata.sql"}, executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
+public class ControllerTest {
+	
+	@Autowired
+	private MockMvc mvc;
+	
+	@Autowired
+	private ObjectMapper mapper;
+	
+	//test for create
+	@Test
+	void createCarTest() throws Exception{
+		String testProductAsJSON = this.mapper.writeValueAsString(new Product(null, "bacon", 3, 8));
+		String testProductAsJSONResponse = this.mapper.writeValueAsString(new Product(3, "bacon", 3, 8));
+		
+		RequestBuilder request = MockMvcRequestBuilders.post("/products").contentType(MediaType.APPLICATION_JSON).content(testProductAsJSON);
+		ResultMatcher status = MockMvcResultMatchers.status().isCreated();
+		ResultMatcher content = MockMvcResultMatchers.content().json(testProductAsJSONResponse);
+		this.mvc.perform(request).andExpect(status).andExpect(content);
+	}
+	//test for delete
+	@Test 
+	void deleteProductTest() throws Exception {
+		RequestBuilder delete = MockMvcRequestBuilders.delete("/products/1");
+		ResultMatcher status = MockMvcResultMatchers.status().isOk();
+		
+		this.mvc.perform(delete).andExpect(status);
+	}
+	//test for update
+	@Test 
+	void updateProductTest() throws Exception {
+		String updatedProductAsJSON = this.mapper.writeValueAsString(new Product(2, "milkshake", 5, 2));
+		
+		RequestBuilder request = MockMvcRequestBuilders.put("/products/2").contentType(MediaType.APPLICATION_JSON).content(updatedProductAsJSON);
+		
+		ResultMatcher status = MockMvcResultMatchers.status().isAccepted();
+		ResultMatcher content = MockMvcResultMatchers.content().json(updatedProductAsJSON);
+		
+		this.mvc.perform(request).andExpect(status).andExpect(content);
+	}
+	//test for get
+	@Test
+	void getAllProductsTest() throws Exception {
+		String listOfProductsAsJSON = this.mapper.writeValueAsString(List.of(
+				new Product(1, "eggs", 2, 12),
+				new Product(2, "chips", 1, 30)
+				));
+		
+		RequestBuilder request = MockMvcRequestBuilders.get("/products");
+		
+		ResultMatcher status = MockMvcResultMatchers.status().isOk();
+		ResultMatcher content = MockMvcResultMatchers.content().json(listOfProductsAsJSON);
+		
+		
+		this.mvc.perform(request).andExpect(status).andExpect(content);
+	}
+	
+	
 }
