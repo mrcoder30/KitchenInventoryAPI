8a03d06d1d48c5ba742bd2b6672ba1587b16746b
Daniel <danielw288@icloud.com>
2021-12-03 12:39:03 +0000

    fixed errors

--- src/test/java/com/valhallastudios/Kitcheninvmanager/controllertest/ControllerTest.java
+++ src/test/java/com/valhallastudios/Kitcheninvmanager/controllertest/ControllerTest.java
@@ -2,8 +2,6 @@ package com.valhallastudios.Kitcheninvmanager.controllertest;
 
 import java.util.List;
 
-import javax.swing.text.AbstractDocument.Content;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -19,7 +17,6 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.valhallacoders.kitcheninventory.model.Product;
 
