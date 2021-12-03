100f098b152d5e1ca02f98637d49474389b6476a
Daniel <danielw288@icloud.com>
2021-12-03 01:23:13 +0000

    delete api calls completed

--- src/main/java/com/valhallacoders/kitcheninventory/controller/ProductController.java
+++ src/main/java/com/valhallacoders/kitcheninventory/controller/ProductController.java
@@ -1,6 +1,12 @@
 package com.valhallacoders.kitcheninventory.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.valhallacoders.kitcheninventory.model.Product;
 
 public class ProductController {
 	
@@ -10,5 +16,21 @@ public class ProductController {
 	// list all products
 	
 	
-	
+
+    @DeleteMapping("/products/{id}")
+    public ResponseEntity<?> delete(@PathVariable Integer id) {
+        boolean deleted = this.service.delete(id);
+        if(deleted) {
+            return new ResponseEntity<>(HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+	@DeleteMapping("/products")
+	public void deleteAll(Product product) {
+		service.deleteAll(product);
+	}
+}
+
 }
