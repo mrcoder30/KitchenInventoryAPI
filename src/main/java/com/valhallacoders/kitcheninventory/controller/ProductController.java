97f6c73ae07c18f762fb197864573625f5e4dd4e
Daniel <danielw288@icloud.com>
2021-12-03 00:54:02 +0000

    service layer completed

--- src/main/java/com/valhallacoders/kitcheninventory/controller/ProductController.java
+++ src/main/java/com/valhallacoders/kitcheninventory/controller/ProductController.java
@@ -1,5 +1,14 @@
-package com.valhallastudios.Kitcheninvmanager.controller;
+package com.valhallacoders.kitcheninventory.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
 
 public class ProductController {
+	
+	
+	
 
+	// list all products
+	
+	
+	
 }
