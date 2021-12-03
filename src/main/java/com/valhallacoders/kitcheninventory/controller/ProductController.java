58c58f46272feba43da1d6cd1f67c4cf55cfc1b8
Daniel <danielw288@icloud.com>
2021-12-03 01:18:23 +0000

    update api call finished

--- src/main/java/com/valhallacoders/kitcheninventory/controller/ProductController.java
+++ src/main/java/com/valhallacoders/kitcheninventory/controller/ProductController.java
@@ -1,13 +1,25 @@
 package com.valhallacoders.kitcheninventory.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import com.valhallacoders.kitcheninventory.model.Product;
 
 public class ProductController {
 	
 	
-	
+	@PutMapping("/products/{id}")
+	public ResponseEntity<Product> update(@RequestBody Product product, @PathVariable Integer id) {
+		ResponseEntity<Product> response = new ResponseEntity<Product>(this.service.update(product, id),
+				HttpStatus.ACCEPTED); //
+		return response;
+	}	
 
-	// list all products
+
 	
 	
 	
