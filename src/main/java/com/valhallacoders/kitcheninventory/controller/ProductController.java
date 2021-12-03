7d8c55f13c8fba68cf6cd2600bdd1508100de536
Daniel <danielw288@icloud.com>
2021-12-03 01:02:09 +0000

    create feature api calls

---
+++ src/main/java/com/valhallacoders/kitcheninventory/controller/ProductController.java
@@ -0,0 +1,52 @@
+package com.valhallacoders.kitcheninventory.controller;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import com.valhallacoders.kitcheninventory.model.Product;
+import com.valhallacoders.kitcheninventory.repository.ProductRepository;
+import com.valhallacoders.kitcheninventory.service.ProductService;
+
+
+@RestController
+public class ProductController {
+
+	// injection of productservice instance
+	@Autowired
+	private ProductService service;
+
+	// list all products
+	@GetMapping("/products")
+	public List<Product> list() {
+		return service.listAll();
+	}
+
+	@GetMapping("/products/{id}")
+	public ResponseEntity<Product> get(@PathVariable Integer id) {
+		try {
+			Product product = service.get(id);
+			return new ResponseEntity<Product>(product, HttpStatus.OK);
+		} catch (NoSuchElementException e) {
+			return new ResponseEntity<Product>(HttpStatus.NOT_FOUND);
+		}
+	}
+}
